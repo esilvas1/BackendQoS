281b1897c0892a5bd361f7f89f88576e09b73eea
EDWIN SILVA SALAS <EDWIN.SILVA@cens.com.co>
2023-04-17 20:33:55 -0500

    Creacion del objeto tabla QA_TTC1_AUTOGEN

---
+++ Tables/QA_TTC1_AUTOGEN.sql
@@ -0,0 +1,6 @@
+CREATE TABLE QA_TTC1_AUTOGEN (
+                                TC1_TC1         VARCHAR2(20),
+                                TC1_CAPAUTOGENR VARCHAR2(6),
+                                TC1_FENTGEN     DATE
+                             )
+;
