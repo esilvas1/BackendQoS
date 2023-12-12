69407b1b50d01fc38087814f7e22f97a23672555
EDWIN SILVA SALAS <EDWIN.SILVA@cens.com.co>
2023-12-12 11:00:27 -0500

    Se crea la tabla QA_TFDDCARGA_SP7_TEMP para cargar los archivos descargados de la aplicacion JASPER REPORT

---
+++ Tables/Formato_DR/QA_TFDDCARGA_SP7_TEMP.sql
@@ -0,0 +1,27 @@
+
+CREATE TABLE BRAE.QA_TFDDCARGA_SP7_TEMP(
+FDD_IDE_Evento VARCHAR2(40)
+,FDD_IDE_Circuito VARCHAR2(40)
+,FDD_Elemento_Falla VARCHAR2(40)
+,FDD_Nodo VARCHAR2(40)
+,FDD_Tipo_Elemento VARCHAR2(40)
+,FDD_Maniobra_Apertura VARCHAR2(40)
+,FDD_Maniobra_Cierre VARCHAR2(40)
+,FDD_Fecha_Desenergizacion DATE
+,FDD_Fecha_Energizacion DATE
+,FDD_Transformador  VARCHAR2(40)
+,FDD_Clientes  NUMBER
+,FDD_Descripcion_Codigo VARCHAR2(400)
+,FDD_Codigo NUMBER
+,FDD_Duracion DATE
+,FDD_UiTi NUMBER
+,FDD_Area_Responsabilidad VARCHAR2(40)
+,FDD_Estado_Evento VARCHAR2(40)
+,FDD_Usuario_Creador VARCHAR2(40)
+,FDD_Usuario_Modificacion VARCHAR2(40)
+,FDD_Trafos_Afectados NUMBER
+);
+
+/
+
+
